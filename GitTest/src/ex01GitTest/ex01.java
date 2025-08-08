94a81773d4350196fcc7f72c03ceb382a44a4919
Namho <skagh9696@gmail.com>
2025-08-08 12:29:18 +0900

    커밋

--- GitTest/src/ex01GitTest/ex01.java
+++ GitTest/src/ex01GitTest/ex01.java
@@ -3,20 +3,14 @@ package ex01GitTest;
 public class ex01 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		System.out.println("안녕하세요 팀원 여러분들");
-
-				
-		System.out.println("123456789");
-=======
-
 
 	
 		System.out.println("나는 팀원 고문석이다.");
 
 		
          System.out.println("나는 팀원 국승균 입니다");
->>>>>>> 3581b2073459d523b30240e22d0e8b57f9a5674c
 		
 	
 		System.out.println("저는 팀원 김다혜입니다.");
