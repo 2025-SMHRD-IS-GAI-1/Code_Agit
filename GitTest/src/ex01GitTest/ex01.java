d979a124c9192a0b8431c83f27dd8d4fe4ae3338
Namho <skagh9696@gmail.com>
2025-08-08 11:59:38 +0900

    확인용

--- GitTest/src/ex01GitTest/ex01.java
+++ GitTest/src/ex01GitTest/ex01.java
@@ -3,8 +3,7 @@ package ex01GitTest;
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-}
+		System.out.println("안녕하세요 팀원 여러분들");
+		
+	}	
+} 
