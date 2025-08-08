3c8e601adf6110a41bd43df041edcb05b8f4deeb
Ko-Mun-Seok <kmseok01@gmail.com>
2025-08-08 12:12:31 +0900

    안녕하세요

--- GitTest/src/ex01GitTest/ex01.java
+++ GitTest/src/ex01GitTest/ex01.java
@@ -3,17 +3,17 @@ package ex01GitTest;
 public class ex01 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
+
 		
 		System.out.println("나는 팀원 고문석이다.");
-=======
+
 		// TODO Auto-generated method stub
 				
 		System.out.println("123456789");
 		
 	
 		System.out.println("저는 팀원 김다혜입니다.");
->>>>>>> f5c3ad994e0399828a93f1e206aa9f7ae1f09225
+
 
 	}
 
