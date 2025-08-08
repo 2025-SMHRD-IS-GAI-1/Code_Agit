f7f988edc40ef937b3e683cdf9c27ac6dee52856
gukseungkyoon <szz123091@gmail.com>
2025-08-08 11:55:56 +0900

    안녕하세요

--- GitTest/src/ex01GitTest/ex01.java
+++ GitTest/src/ex01GitTest/ex01.java
@@ -3,8 +3,8 @@ package ex01GitTest;
 public class ex01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+         System.out.println("나는 팀원 국승균 입니다");
 	}
 
 }
